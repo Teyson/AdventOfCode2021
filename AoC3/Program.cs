eff07a88293486f8997a350e9c276f4de76592f5
Teyson <teis@aalbaek-nielsen.dk>
2021-12-07 00:13:16 +0100

    Project structure for day 3

---
+++ AoC3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using AoC3;
+
+
+string[] lines = System.IO.File.ReadAllLines("C:\\Users\\teis\\OneDrive\\Dokumenter\\Programmering\\AdventOfCode2021\\AoC3\\input1.txt");
+
+
+namespace AoC3
+{
+    
+}
